d5b0969c49e1ddd79cd75d257272942db9b7a267
Kenneth Ofosu <kofosu89@gmail.com>
2020-04-10 13:21:36 -0400

    auth up to date on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+
+  def secret
+    Rails.application.credentials.my_app_secret
+  end
+
+  def encode_token(payload)
+    JWT.encode payload, secret, 'HS256'
+  end
+
+  def user_payload(user)
+    { user_id: user.id }
+  end
+
+  def token
+    request.headers["Authorization"]
+  end
+
+  def decoded_token
+    JWT.decode token, secret, true, { algorithm: 'HS256' }
+  end
+
+  def current_user
+    User.find(decoded_token[0]["user_id"])
+  end
 end
