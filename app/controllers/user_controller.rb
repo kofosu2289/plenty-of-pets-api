11fa33ddfb5cab4424c65c7db7878ecf1cb3a73e
Kenneth Ofosu <kofosu89@gmail.com>
2020-04-10 15:20:06 -0400

    update user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,23 +1,28 @@
 class UserController < ApplicationController
 
-  def create
-    user = User.create(user_params)
-    # byebug
-    if user.valid?
-      render json: { token: encode_token(user_payload(user)) }
-    else
-      render json: { errors: user.errors.full_messages }
-    end
-  end
+	def create
+		user = User.create(user_params)
+		# byebug
+		if user.valid?
+			render json: {token: encode_token(user_payload(user))}
+		else
+			render json: { errors: user.errors.full_messages }
+		end
+	end
 
-  def profile
-    # byebug
-    render json: current_user
-  end
+	# def show
+	# 	@user = User.find(params[:id])
+	# 	render :show
+	# end
 
-  private
+	def profile
+		# byebug
+		render json: current_user 
+	end
 
-  def user_params
-    params.permit(:username, :password, :first_name, :last_name, :email)
-  end
+	private 
+
+	def user_params
+		params.permit(:username, :password, :first_name, :last_name, :email)
+	end
 end
