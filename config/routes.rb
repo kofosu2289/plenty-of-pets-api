873f81396412ac84dba6b1c55dbfdecaf2e392c2
Kenneth Ofosu <kofosu89@gmail.com>
2020-04-10 17:19:02 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/login", to: "auth#login"
-  post "/signup", to: "user#create"
-  get "/profile", to: "user#profile"
+  post "/signup", to: "users#create"
+  get "/profile", to: "users#profile"
 end
